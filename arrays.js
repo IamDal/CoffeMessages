b684ee4f5c80d3d55a656b62a341a210f6aec2b7
Dalton Burton <daltonburton16@gmail.com>
2024-07-11 20:57:24 +0900

    update arrays in arrays.js

--- arrays.js
+++ arrays.js
@@ -1,4 +1,32 @@
-export const coffee = [];
-export const greeting = [];
-export const coffeFacts = [];
+export const coffee = [
+    "Jamaican Blue Mountain Coffee",
+    "Kenyan Coffee",
+    "Colombian Coffee",
+    "Brazilian Coffee",
+    "Ethiopia Coffee",
+    "Coffee",
+    "Honduran Coffee"
+];
+
+export const greeting = [
+    "Did you know",
+    "Quick fact",
+    "Your daily tip",
+    "Guess what",
+    "Its amazing that",
+    "Good news",
+];
+
+export const coffeFacts = [
+"drinkers may enjoy more overall health benefits than people who don't drink coffee",
+"drinkers were 17% less likely to die early from any cause, 19% less likely to die of heart disease and 18% less likely to develop cancer than those who don't drink coffee",
+"drinking has beneficial effects on the liver",
+"in the morning could help lower your risk of death",
+"contains caffeine, a stimulant that has been shown to increase energy levels and decrease fatigue by altering levels of certain neurotransmitters in the brain.",
+"consumption may be linked to a lower risk of developing type 2 diabetes over the long term.",
+"could help protect against Alzheimer’s disease, Parkinson’s disease, and cognitive decline.",
+"could help support weight management and may be linked to decreased body fat.",
+"could be linked to a lower risk of depression and may even be linked to a lower risk of death by suicide.",
+"could improve physical performance and endurance when consumed before exercising."
+];
 export const closing = [];
