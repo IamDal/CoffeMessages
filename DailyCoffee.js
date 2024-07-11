a6b8a74fcc51d16b27eac512960546d4876c6de2
Dalton Burton <daltonburton16@gmail.com>
2024-07-11 21:27:17 +0900

    Add package.json to allow imports \nAdd random messages to main function

--- DailyCoffee.js
+++ DailyCoffee.js
@@ -1,8 +1,18 @@
-function dailyCoffee(userName) {
-    const coffee = [];
-    const greeting = [];
-    const coffeFacts = [];
-    const closing = [];
-    
+import {
+    coffee,
+    greeting,
+    coffeFacts,
+    closing
+} from './arrays.js';
 
-}
+function dailyCoffee(userName) {
+    let randCoffee = Math.floor(Math.random()* coffee.length);
+    let randGreeting = Math.floor(Math.random()* greeting.length);
+    let randFact = Math.floor(Math.random()* coffeFacts.length);
+    let randClose = Math.floor(Math.random()* closing.length);
+    return `${greeting[randGreeting]} ${userName} ${coffee[randCoffee]} ${coffeFacts[randFact]} ${closing[randClose]}`;   
+};
+
+// Test case
+
+console.log(dailyCoffee('dalton'));
