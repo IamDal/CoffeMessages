31258b1e7863761d0e980844c4e9ebc36163c708
Dalton Burton <daltonburton16@gmail.com>
2024-07-11 20:59:26 +0900

    update arrays in arrays.js

--- arrays.js
+++ arrays.js
@@ -18,15 +18,21 @@ export const greeting = [
 ];
 
 export const coffeFacts = [
-"drinkers may enjoy more overall health benefits than people who don't drink coffee",
-"drinkers were 17% less likely to die early from any cause, 19% less likely to die of heart disease and 18% less likely to develop cancer than those who don't drink coffee",
-"drinking has beneficial effects on the liver",
-"in the morning could help lower your risk of death",
-"contains caffeine, a stimulant that has been shown to increase energy levels and decrease fatigue by altering levels of certain neurotransmitters in the brain.",
-"consumption may be linked to a lower risk of developing type 2 diabetes over the long term.",
-"could help protect against Alzheimer’s disease, Parkinson’s disease, and cognitive decline.",
-"could help support weight management and may be linked to decreased body fat.",
-"could be linked to a lower risk of depression and may even be linked to a lower risk of death by suicide.",
-"could improve physical performance and endurance when consumed before exercising."
+    "drinkers may enjoy more overall health benefits than people who don't drink coffee",
+    "drinkers were 17% less likely to die early from any cause, 19% less likely to die of heart disease and 18% less likely to develop cancer than those who don't drink coffee",
+    "drinking has beneficial effects on the liver",
+    "in the morning could help lower your risk of death",
+    "contains caffeine, a stimulant that has been shown to increase energy levels and decrease fatigue by altering levels of certain neurotransmitters in the brain.",
+    "consumption may be linked to a lower risk of developing type 2 diabetes over the long term.",
+    "could help protect against Alzheimer’s disease, Parkinson’s disease, and cognitive decline.",
+    "could help support weight management and may be linked to decreased body fat.",
+    "could be linked to a lower risk of depression and may even be linked to a lower risk of death by suicide.",
+    "could improve physical performance and endurance when consumed before exercising."
 ];
-export const closing = [];
+export const closing = [
+    "Java nice day!",
+    "Have a good day, brew can do it!",
+    "Go out and expresso your self!",
+    "Enjoy your day, just brew it!",
+    "Stay grounded"
+];
