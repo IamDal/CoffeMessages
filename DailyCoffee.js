254f6455809c6b4271b84a0f462b423eb9a4d98e
Dalton Burton <daltonburton16@gmail.com>
2024-07-12 10:01:59 +0900

    create readme file

--- DailyCoffee.js
+++ DailyCoffee.js
@@ -1,3 +1,9 @@
+/*  -- Random Coffee Facts Generator --
+    This script outputs a general random fact about coffee. It suggests
+    a type of coffee and outputs ascii art as a visual touch.
+*/
+
+// Import data arrays from arrays.js
 import {
     coffee,
     greeting,
@@ -6,15 +12,19 @@ import {
     art,
 } from './arrays.js';
 
+// Define main function
 function dailyCoffee() {
+
+    // Randomly select an element of each array
     let randCoffee = Math.floor(Math.random()* coffee.length);
     let randGreeting = Math.floor(Math.random()* greeting.length);
     let randFact = Math.floor(Math.random()* coffeFacts.length);
     let randClose = Math.floor(Math.random()* closing.length);
-    let tab = "                     "
-    return `${greeting[randGreeting]} ${coffee[randCoffee]} ${coffeFacts[randFact]}\n${art.join("\n")} ${tab}${closing[randClose]}`;   
+    let tab = "                     ";
+
+    // Return concatenated string
+    return `${greeting[randGreeting]} ${coffee[randCoffee]} ${coffeFacts[randFact]}\n ${art.join("\n")} ${tab}${closing[randClose]}`;   
 };
 
-// Test case
-
+// Display random message to console
 console.log(dailyCoffee());
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Coffee Message Generator
+#### by Dalton R. Burton
+This is a script that generates random coffee facts each time it is run by the user.
+
+## Contents
+1. Installation
+2. How to use
+3. Screenshots
+
+## Installation
+The following is a general guide on how to install the script
+
+### Requirements
+You need:
+Node.js (version v18.15.0 or later)
+** link node download here
+Make a clone of this repository:
+** link repository here
+
+You can download files locally or run on a cloud codespace.
+
+## How to use
+1. Once files are ready, navigate to the project folder 'CoffeeMessages'
+2. execute the following command in the terminal:
+    node DailyCoffee.js
+3. That's it enjoy the coffee!
